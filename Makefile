e2220924bbd0954d4bb3a99bfa37038ff117be56
DanDovhun <dankodov@gmail.com>
2023-03-07 16:14:00 +0100

    Deleted

--- Makefile
+++
@@ -1,9 +0,0 @@
-build:
-	go build -o ~/Desktop/BudgetManager/bin
-
-run:
-	go run main.go
-
-build-and-run:
-	make build
-	./BudgetManager
--- funcs/funcs.go
+++
@@ -1,9 +0,0 @@
-package funcs
-
-import (
-	"math"
-)
-
-func Round(num float64, to int) float64 {
-	return math.Round(num*math.Pow10(to)) / math.Pow10(to)
-}
