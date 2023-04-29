a1fdf35faab9974dec163ebb99ffebf0eebb2833
DannyDov <39409234+DanDovhun@users.noreply.github.com>
2023-04-29 21:46:26 +0200

    Added comments (#17)

--- datamng/Expense.go
+++ datamng/Expense.go
@@ -14,10 +14,12 @@ type Expense struct {
 	Date         string
 }
 
+// Creates a new expense, it's essentially a constructor for the Expense struct
 func NewExpense(title, category, denomination string, price float64) Expense {
-	currentTime := time.Now()
-	year, month, day := currentTime.Date()
+	currentTime := time.Now()              // Get current time
+	year, month, day := currentTime.Date() // Get current date
 
+	// Setup and return a new expense
 	return Expense{
 		Name:         title,
 		Category:     category,
@@ -28,6 +30,7 @@ func NewExpense(title, category, denomination string, price float64) Expense {
 	}
 }
 
+// Formats the Expense into a string so it could be added to a CSV file
 func (ex Expense) FormatToCSV() string {
 	return fmt.Sprintf("%v,%v,%v,%v,%v", ex.Name, ex.Category, ex.Price, ex.Denomination, ex.Date)
 }
--- datamng/Manage.go
+++ datamng/Manage.go
@@ -7,27 +7,31 @@ import (
 	"time"
 )
 
+// Adds a new expense to the database
 func Add(expense Expense) error {
-	data, err := os.Open("expenses.json")
-	var months Data
+	data, err := os.Open("expenses.json") // Open the json file
+	var months Data                       // The loaded data will be stored here
 
+	// If the file couldn't be opened
 	if err != nil {
-		return err
+		return err // Return the error
 	}
 
-	defer data.Close()
+	defer data.Close() // close the file when the function ends
 
-	byteValue, _ := ioutil.ReadAll(data)
-	json.Unmarshal(byteValue, &months)
+	byteValue, _ := ioutil.ReadAll(data) //Read the file as []bytes
+	json.Unmarshal(byteValue, &months)   // Store the bytes in the months
 
-	currentYear, currentMonth, _ := time.Now().Date()
+	currentYear, currentMonth, _ := time.Now().Date() // Get current year and month
 
-	monthIndex := months.FindMonthByYear(currentMonth, currentYear)
+	monthIndex := months.FindMonthByYear(currentMonth, currentYear) // Check where current month is and if it exists
 
+	// If the month doesn't yet exist (the new expense is first that month)
 	if monthIndex == -1 {
+		// Add the month to the database
 		months.Months = append(months.Months, Month{
-			Budget:        months.Budget,
-			TotalSpending: 0,
+			Budget:        months.Budget, // Make that months budget equal to the current budget
+			TotalSpending: 0,             // Set total spending to 0
 
 			Year: currentYear,
 			Moon: currentMonth,
@@ -40,10 +44,14 @@ func Add(expense Expense) error {
 			Miscelanious: []Expense{},
 		})
 
-		monthIndex = months.FindMonthByYear(currentMonth, currentYear)
+		// The index of that month will be the last in the database
+		monthIndex = len(months.Months) - 1
 	}
 
+	//Increment that month's spending by the expense's cost
 	months.Months[monthIndex].TotalSpending += expense.Price
+
+	// Sort the expense into its category
 	switch expense.Category {
 	case "Groceries":
 		months.Months[monthIndex].Groceries = append(months.Months[monthIndex].Groceries, expense)
@@ -64,8 +72,10 @@ func Add(expense Expense) error {
 		months.Months[monthIndex].Miscelanious = append(months.Months[monthIndex].Miscelanious, expense)
 	}
 
+	// Format the update database and write it to the json file
 	j, _ := json.MarshalIndent(months, "", "    ")
 	_ = ioutil.WriteFile("expenses.json", j, 0644)
 
+	// Do not return errors
 	return nil
 }
